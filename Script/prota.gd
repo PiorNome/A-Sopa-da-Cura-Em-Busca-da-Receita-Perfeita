245a2597ebf2c3e07e9f372ea5363781a484e30a
Pior Nome <lukeloke007@gmail.com>
2026-08-06 17:14:26 -0300

    Primeiro Commit do Jogo
    O personagem anda! 👍

---
+++ Script/prota.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var velocity = Vector2.ZERO
+var speed = 250
+var screensize = Vector2(1920, 1080)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screensize = get_viewport_rect().size
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	velocity = Input.get_vector("andar_esquerda", "andar_direita", "andar_cima", "andar_baixo")
+	position += velocity * delta * speed
+	
+	if velocity.length() > 0:
+		if velocity.x > 0:
+			$AnimatedSprite2D.animation = "Direito"
+		elif velocity.x < 0:
+			$AnimatedSprite2D.animation = "Esquerda"
+		elif velocity.y > 0:
+			$AnimatedSprite2D.animation = "Baixo"
+		elif velocity.y < 0:
+			$AnimatedSprite2D.animation = "Cima"
+		
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.frame = 0
+	
+	pass
