02e8e8e20b3b52584cbbf307de790853b27c5572
Pior Nome <lukeloke007@gmail.com>
2026-08-19 14:23:59 -0300

    Nome adicionado no balão de fala

--- Script/ui.gd
+++ Script/ui.gd
@@ -1,6 +1,6 @@
 extends Control
 
-func mudar_fala(texto_fala: String = "", cor_borda: Color = Color(0.30, 0.50, 0.52, 1.00), cor_fundo: Color = Color(0,0,0,1), imagem_personagem: ImageTexture = null
+func mudar_fala(nome:String="???", texto_fala: String = "", cor_borda: Color = Color(0.30, 0.50, 0.52, 1.00), cor_fundo: Color = Color(0,0,0,1), imagem_personagem: ImageTexture = null
 	): # :(
 	$Label.text = texto_fala
 	if not imagem_personagem == null:
@@ -10,3 +10,10 @@ func mudar_fala(texto_fala: String = "", cor_borda: Color = Color(0.30, 0.50, 0.
 	
 	stylebox.bg_color = cor_fundo
 	stylebox.border_color = cor_borda
+	
+	var stylebox_nome: StyleBoxFlat = $Nome.get_theme_stylebox("nome")
+	
+	stylebox_nome.bg_color = cor_fundo
+	stylebox_nome.border_color = cor_borda
+	
+	$Nome/Label.text = nome
