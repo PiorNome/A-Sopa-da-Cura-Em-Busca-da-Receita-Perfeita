78a4720effb5dc8f2476323b9a6c0e497adf17cc
Pior Nome <lukeloke007@gmail.com>
2026-09-21 15:07:46 -0300

    Colisões Arrumadas
    Eu adicionei um colisão para receber dano.
    E colisão no mapa "map.tscn"

--- Cenas/Personagens/prota.gd
+++ Cenas/Personagens/prota.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const JUMP_VELOCITY = -400.0
 const SPEED = 150
+var vida = 3
 
 
 
@@ -31,3 +32,7 @@ func _physics_process(_delta: float) -> void:
 
 	# Move o personagem e lida com as colisões do TileMap automaticamente
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
