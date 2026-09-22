1709855759503b9806a94c8b3af0611f0763c395
Pior Nome <lukeloke007@gmail.com>
2026-09-22 09:27:11 -0300

    Finalizei a detecção de dano

--- Cenas/Personagens/prota.gd
+++ Cenas/Personagens/prota.gd
@@ -35,4 +35,8 @@ func _physics_process(_delta: float) -> void:
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	if area.is_in_group("Ataque_Inimigo"):
+		vida -= 1
+		
+		if vida == 0:
+			pass # Cena de game over
